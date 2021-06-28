5c55cd330e58f13171cd2ea605b671e2557cbaf6
Roch-Jacquot <roch.jacquot@gmail.com>
2021-06-28 17:32:33 +0200

    added some comments InDB

--- src/main/java/fr/eql/comments/init/InitDataSet.java
+++ src/main/java/fr/eql/comments/init/InitDataSet.java
@@ -21,21 +21,41 @@ public class InitDataSet {
 	
 	@PostConstruct
 	public void initDemoData() {
-		Comment comment1Id1TypeGROUP = new Comment(1L, "This is a comment for entity 1 and Type GROUP", "Billy", "GROUP", LocalDateTime.of(2021, 4, 14, 15, 10));
-		Comment comment2Id1TypeGROUP = new Comment(1L, "This is another for entity 1 and Type GROUP", "Bobby", "GROUP", LocalDateTime.of(2021, 4, 14, 16, 10));
 		
-		Comment comment1Id2TypeGROUP = new Comment(2L, "This is a comment for entity 2 and Type GROUP", "Mimi", "GROUP", LocalDateTime.of(2021, 4, 14, 15, 15));
-		Comment comment2Id2TypeGROUP = new Comment(2L, "This is another for entity 2 and Type GROUP", "Zazy", "GROUP", LocalDateTime.of(2021, 4, 14, 16, 30));
+		//Init of comments for GROUP 1
+		Comment Group1Commentaire1 = new Comment(1L, "Welcome to our group guys", "mimi92", "GROUP", LocalDateTime.of(2021, 05, 11, 11, 15));
+		Comment Group1Commentaire2 = new Comment(1L, "Thanks for inviting me !!!!", "matteo le dev", "GROUP", LocalDateTime.of(2021, 05, 11, 13, 15));
 		
-		Comment comment1Id1TypeTICKET = new Comment(1L, "This is a comment for entity 1 and Type TICKET", "Corbeille", "TICKET", LocalDateTime.of(2021, 4, 15, 15, 10));
-		Comment comment2Id2TypeTICKET = new Comment(2L, "This is another for entity 2 and Type TICKET", "Mordred", "TICKET", LocalDateTime.of(2021, 4, 14, 16, 10));
+		Comment Group1Commentaire3 = new Comment(1L, "Do you wanna develop an app ?", "mimi92", "GROUP", LocalDateTime.of(2021, 05, 14, 15, 15));
+		Comment Group1Commentaire4 = new Comment(1L, "No thanks mimi92", "matteo le dev", "GROUP", LocalDateTime.of(2021, 05, 15, 16, 30));
+		Comment Group1Commentaire5 = new Comment(1L, "What's a 'ticketing app' ?", "devMy75", "GROUP", LocalDateTime.of(2021, 05, 15, 16, 30));
+		commentService.save(Group1Commentaire1);
+		commentService.save(Group1Commentaire2);
+		commentService.save(Group1Commentaire3);
+		commentService.save(Group1Commentaire4);
+		commentService.save(Group1Commentaire5);
+	
+		//Init comments for Tickets related to group1 (1 to 9) 
+//		LocalDateTime.of(2021, 4, 14, 15, 10)
+//		LocalDateTime.of(2021, 4, 14, 15, 15)
+//		LocalDateTime.of(2021, 04, 14, 15, 20)
 		
-		commentService.save(comment1Id1TypeGROUP);
-		commentService.save(comment2Id1TypeGROUP);
-		commentService.save(comment1Id2TypeGROUP);
-		commentService.save(comment2Id2TypeGROUP);
-		commentService.save(comment1Id1TypeTICKET);
-		commentService.save(comment2Id2TypeTICKET);		
+		Comment Ticket1Commentaire1 = new Comment(1L, "I need to find some infos", "mimi92", "TICKET", LocalDateTime.of(2021, 05, 15, 15, 10));
+		Comment Ticket1Commentaire2 = new Comment(1L, "Check Stackoverflow!", "mimi92", "TICKET", LocalDateTime.of(2021, 05, 15, 15, 30));
+		
+		Comment Ticket2Commentaire1 = new Comment(2L, "Hey, about the database I think we should use StarUML, what do you think ?", "matteo le dev", "TICKET", LocalDateTime.of(2021, 5, 15, 17, 10));
+		Comment Ticket2Commentaire2 = new Comment(2L, "I have more experience with PowerAMC, can we use it instead ?", "devMy75", "TICKET", LocalDateTime.of(2021, 5, 15, 18, 10));
+		Comment Ticket2Commentaire3 = new Comment(2L, "Sure, no problems.", "matteo le dev", "TICKET", LocalDateTime.of(2021, 5, 16, 17, 10));
+		Comment Ticket2Commentaire4 = new Comment(2L, "Check the discord, I posted a preliminary model", "devMy75", "TICKET", LocalDateTime.of(2021, 5, 17, 18, 10));
+		
+		commentService.save(Ticket1Commentaire1);		
+		commentService.save(Ticket1Commentaire2);
+		commentService.save(Ticket2Commentaire1);		
+		commentService.save(Ticket2Commentaire2);
+		commentService.save(Ticket2Commentaire3);		
+		commentService.save(Ticket2Commentaire4);
+		
+		
 	}
 
 }
